63cedcdf4e7867ad698ae0e0604dc5d106b62f19
Karthik hr <109428613+karthikhrd@users.noreply.github.com>
2024-07-01 14:24:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx']
-    }
-  }
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
